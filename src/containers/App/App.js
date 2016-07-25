5b143c375a0704eb667081744ea155576f3f675e
Saurav Kumar <sauravskumar99@gmail.com>
2016-07-25 18:16:48 +0530

    first commit

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,35 +1,36 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { IndexLink } from 'react-router';
-import { LinkContainer } from 'react-router-bootstrap';
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {IndexLink} from 'react-router';
+import {LinkContainer} from 'react-router-bootstrap';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
 import Helmet from 'react-helmet';
-import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
-import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
-import { InfoBar } from 'components';
-import { push } from 'react-router-redux';
+// import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
+// import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
+import {InfoBar} from 'components';
+import {push} from 'react-router-redux';
 import config from '../../config';
-import { asyncConnect } from 'redux-async-connect';
+import {asyncConnect} from 'redux-connect';
 
-@asyncConnect([{
-  promise: ({store: {dispatch, getState}}) => {
-    const promises = [];
+// @asyncConnect([{
+// promise: ({store: {dispatch, getState}}) => {
+//   const promises = [];
 
-    if (!isInfoLoaded(getState())) {
-      promises.push(dispatch(loadInfo()));
-    }
-    if (!isAuthLoaded(getState())) {
-      promises.push(dispatch(loadAuth()));
-    }
+// if (!isInfoLoaded(getState())) {
+//   promises.push(dispatch(loadInfo()));
+// }
+// if (!isAuthLoaded(getState())) {
+//   promises.push(dispatch(loadAuth()));
+// }
 
-    return Promise.all(promises);
-  }
-}])
-@connect(
-  state => ({user: state.auth.user}),
-  {logout, pushState: push})
+// return Promise.all(promises);
+//   }
+// }])
+// @connect(
+// state => ({user: state.auth.user}),
+// {logout, pushState: push}
+// )
 export default class App extends Component {
   static propTypes = {
     children: PropTypes.object.isRequired,
@@ -105,23 +106,18 @@ export default class App extends Component {
             {user &&
             <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
             <Nav navbar pullRight>
-              <NavItem eventKey={1} target="_blank" title="View on Github" href="https://github.com/erikras/react-redux-universal-hot-example">
+              <NavItem eventKey={1} target="_blank" title="View on Github"
+                       href="https://github.com/erikras/react-redux-universal-hot-example">
                 <i className="fa fa-github"/>
               </NavItem>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
 
-        <div className={styles.appContent}>
-          {this.props.children}
-        </div>
-        <InfoBar/>
-
-        <div className="well text-center">
-          Have questions? Ask for help <a
-          href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-          target="_blank">on Github</a> or in the <a
-          href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
+        <div className="container-fluid">
+          <div className={styles.appContent}>
+            {this.props.children}
+          </div>
         </div>
       </div>
     );
--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
-import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
+// import {isLoaded as isAuthLoaded, load as loadAuth} from 'redux/modules/auth';
 import {
-    App,
-    Chat,
-    Home,
-    Widgets,
-    About,
-    Login,
-    LoginSuccess,
-    Survey,
-    NotFound,
-  } from 'containers';
+  App,
+  // Chat,
+  Home,
+  // Widgets,
+  About,
+  // Login,
+  // LoginSuccess,
+  // Survey,
+  NotFound,
+} from 'containers';
 
-export default (store) => {
-  const requireLogin = (nextState, replace, cb) => {
-    function checkAuth() {
-      const { auth: { user }} = store.getState();
-      if (!user) {
-        // oops, not logged in, so can't be here!
-        replace('/');
-      }
-      cb();
-    }
-
-    if (!isAuthLoaded(store.getState())) {
-      store.dispatch(loadAuth()).then(checkAuth);
-    } else {
-      checkAuth();
-    }
-  };
+export default () => {
+// export default (store) => {
+  // const requireLogin = (nextState, replace, cb) => {
+  //   function checkAuth() {
+  //     const { auth: { user }} = store.getState();
+  //     if (!user) {
+  //       // oops, not logged in, so can't be here!
+  //       replace('/');
+  //     }
+  //     cb();
+  //   }
+  //
+  //   if (!isAuthLoaded(store.getState())) {
+  //     store.dispatch(loadAuth()).then(checkAuth);
+  //   } else {
+  //     checkAuth();
+  //   }
+  // };
 
   /**
    * Please keep routes in alphabetical order
@@ -40,19 +41,19 @@ export default (store) => {
       <IndexRoute component={Home}/>
 
       { /* Routes requiring login */ }
-      <Route onEnter={requireLogin}>
+      { /* <Route onEnter={requireLogin}>
         <Route path="chat" component={Chat}/>
         <Route path="loginSuccess" component={LoginSuccess}/>
-      </Route>
+      </Route> */ }
 
       { /* Routes */ }
       <Route path="about" component={About}/>
-      <Route path="login" component={Login}/>
+      {/* <Route path="login" component={Login}/>
       <Route path="survey" component={Survey}/>
-      <Route path="widgets" component={Widgets}/>
+      <Route path="widgets" component={Widgets}/> */}
 
       { /* Catch all route */ }
-      <Route path="*" component={NotFound} status={404} />
+      <Route path="*" component={NotFound} status={404}/>
     </Route>
   );
 };
